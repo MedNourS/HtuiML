f3beb72973c5bff4462c4c41d16fb7660ee20639
Nour Sabir <sabir.med.nour@gmail.com>
2026-03-08 00:57:36 -0500

    Attempting to finish the parser, refactoring to be done

--- HtuiML/parser.py
+++ HtuiML/parser.py
@@ -1,3 +1,5 @@
+import re
+
 def get_html_content(abs_path: str) -> str:
     """
     Returns a string representing the contents of an HTML file
@@ -15,4 +17,18 @@ def minify_html(html: str) -> str:
     Returns a string representing the minified version of an HTML file
     """
     
-    return html.replace("\n", "").replace(" <", "<").replace("> ", ">").replace("< ", "<").replace(" >", ">").replace("  ", " ").replace("  ", " ")
+    return html.replace("\n", "").replace(" <", "<").replace("> ", ">").replace("< ", "<").replace(" >", ">").replace("  ", " ").strip()
+
+def remove_unnecessary_html(html: str) -> str:
+    """
+    Returns a string representing only the content of the <body>, removing comments and whatnot
+    """
+    
+    return re.sub("<!--(.*?)-->", "", re.sub("<head>(.*?)</head>", "", re.sub("<script>(.*?)</script>", "", html.split("<body>")[1].split("</body>")[0]))).strip()
+
+def structure(html: str) -> str:
+    """
+    Returns a string representing the structure of the html's contents, using indents
+    """
+    
+    return html.replace(">", ">\n  ").replace("</", "\n</")
